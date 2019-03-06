54b6e3a35904428fcc5c50305ad679d2c26810c7
Davis, Lauren <ldav7984@740s-pl-109.local>
2019-03-06 12:38:08 -0700

    Initial Commit

---
+++ RecursionProject/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  RecursionProject
+//
+//  Created by Davis, Lauren on 3/6/19.
+//  Copyright © 2019 Davis, Lauren. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
