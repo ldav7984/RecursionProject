7fe0ffea3c2476ff3473819506fbe44f9d637bfe
Davis <ldav7984@740s-pl-109.local>
2019-03-06 13:20:00 -0700

    Created project and committed to Github.

--- RecursionProject/main.cpp
+++ RecursionProject/main.cpp
@@ -7,9 +7,62 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+long fibonacci(long number)
+{
+    //Iterative//
+    if (number <= 1)
+    {
+        return 1;
+    }
+    else
+    {
+        long previous = 1;
+        long current = 1;
+        
+        for(int index = 2; index < number; index++)
+        {
+            long temp = current;
+            current += previous;
+            previous = temp;
+        }
+        return current;
+    }
+    
+//Recursive//
+//    if (number == 0 || number == 1) //base case
+//    {
+//        return 1;
+//    }
+//    else //call
+//    {
+//        return fibonacci(number - 1) + fibonacci(number - 2);
+//    }
+}
+
+double factorial(double number)
+{
+    
+    //Recursive//
+    if (number == 1 || number == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return number * factorial(number - 1);
+    }
+    
+}
+
+int main()
+{
+    for (int index = 50; index < 100; index++)
+    {
+        cout << "The Fibonacci sequence at " << index << " is " << fibonacci(index) << endl;
+        //cout << "The factorial of " << index << " is " << factorial(index) << endl;
+    }
+    
     return 0;
 }
